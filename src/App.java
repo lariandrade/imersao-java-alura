9a35b70199df33e4e77fe15f3be9505fc2b77262
Larissa Andrade <44838761+lariandrade@users.noreply.github.com>
2023-03-29 10:12:33 -0300

    aula 03

--- src/App.java
+++ src/App.java
@@ -1,51 +1,37 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         //fazer conexão HTTP e pegar filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        //ExtratorConteudo extrator = new ExtratorConteudoIMDB();
+        
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/NASA-APOD.json";
+        ExtratorConteudo extrator = new ExtratorConteudoNasa();
 
-        //extrair apenas dados que interessam (titulo, poster e nota)
-        JsonParser parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
         //exibir e manipular dados
+        
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
         var geradora = new GeradorDeFigurinhas();
-        for (Map<String,String> filme : listaDeFilmes) {
 
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
+       for(int i=0; i<3; i++){
+
+            Conteudo conteudo = conteudos.get(i);
            
-            InputStream inputStream = new URL(urlImagem).openStream();
-
-            String nomeArquivo = titulo + ".png";
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = conteudo.getTitulo() + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println("\u001b[1mTitulo: \u001b[m" + filme.get("title"));
-            System.out.println("\u001b[1mImagem: \u001b[m" + filme.get("image"));
-            System.out.println("\u001b[1m\u001b[44;1mNota:  " + filme.get("imDbRating") + "\u001b[m");
-            
-            double classificacao = Double.parseDouble(filme.get("imDbRating"));
-            int qtdEstrelas = (int) classificacao;
-            for(int n=1; n<=qtdEstrelas; n++){
-                System.out.print("⭐");
-            }
-            System.out.println("\n");
+            System.out.println(conteudo.getTitulo());
+            System.out.println();
 
         }   
 
---
+++ src/ExtratorConteudoNasa.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorConteudoNasa implements ExtratorConteudo {
+    
+    public List<Conteudo> extraiConteudos(String json){
+        
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeAtributos = parser.parse(json); 
+
+        List<Conteudo> conteudos = new ArrayList<>();
+        
+        //popular lista de conteudos
+        for (Map<String, String> atributos : listaDeAtributos) {
+            String titulo = atributos.get("title");
+            String urlImagem = atributos.get("url");
+            Conteudo conteudo = new Conteudo(titulo, urlImagem);
+
+            conteudos.add(conteudo);
+        }
+        
+        return conteudos;
+    }
+}
